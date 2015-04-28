071c15e7752952eaab63a02ca0610f455b814c97
Albin Sunnanbo <albin@sunnanbo.com>
2015-04-28 20:04:41 +0200

    Impement BurstFilter

---
+++ Kentor.Log4NetExtensions.BurstFilter/BurstFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.Log4NetExtensions
+{
+    public class BurstFilter : log4net.Filter.FilterSkeleton
+    {
+        public BurstFilter()
+        {
+            BurstLength = TimeSpan.FromMinutes(1);
+            BurstSize = 60;
+        }
+        private static readonly ConcurrentQueue<DateTime> queuedEvents = new ConcurrentQueue<DateTime>();
+        public override log4net.Filter.FilterDecision Decide(log4net.Core.LoggingEvent loggingEvent)
+        {
+            var utcNow = DateTime.UtcNow;
+            queuedEvents.Enqueue(utcNow);
+            DateTime firstTimeUtc = DateTime.MinValue;
+            bool hasEntry = false;
+            while (queuedEvents.Count > BurstSize)
+            {
+                if (queuedEvents.TryDequeue(out firstTimeUtc))
+                {
+                    hasEntry = true;
+                }
+            }
+            if (hasEntry)
+            {
+                if (firstTimeUtc.Add(BurstLength) > utcNow)
+                {
+                    return log4net.Filter.FilterDecision.Deny;
+                }
+            }
+            return log4net.Filter.FilterDecision.Neutral;
+        }
+
+        public TimeSpan BurstLength { get; set; }
+        public int BurstSize { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Log4NetExtensions
 Currently only a BurstFilter for log4net
+
+Example configuration
+
+    <log4net>
+        <appender name="SMTPAppender" type="log4net.Appender.SmtpAppender,log4net">
+          <bufferSize value="1"/>
+          <authentication value="Basic" />
+          <to value="to@example.com" />
+          <from value="from@example.com" />
+          <subject value="Automatmail: system X is totally broken, please fix" />
+          <smtpHost value="smtp.example.com" />
+          <username value="username"/>
+          <password value="password"/>
+          <lossy value="false" />
+          <threshold value="ERROR"/>
+          <filter type="Kentor.Log4NetExtensions.BurstFilter">
+		    <!-- Allow two mails a minute on average, with bursts up to 20 mails -->
+            <BurstLength value="00:10:00"/>
+            <BurstSize value="20"/>
+          </filter>
+          <layout type="log4net.Layout.PatternLayout">
+            <conversionPattern value="%property{log4net:HostName}%newline%date user:%aspnet-request{AUTH_USER} %-5level %logger %newline %message%newline%exception%newline%newline"/>
+          </layout>
+        </appender>
+        <root>
+          <level value="ALL" />
+          <appender-ref ref="SMTPAppender" />
+        </root>
+    </log4net>
